dc345380b7b41cb31efb9059b8b3459e48dc5e3f
Douglas <112734316+Dhrds@users.noreply.github.com>
2023-06-19 18:22:34 -0300

    Atualizar o settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,10 +1,9 @@
 import os
 from pathlib import Path
 
-import mysqlclient
 import info
 
-mysqlclient.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
