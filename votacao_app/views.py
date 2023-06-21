37dc0f5bbd94b5d15e084e296b79d9f1d9021dd1
Douglas <dhrds1996@gmail.com>
2023-06-21 16:21:10 -0300

    1

--- votacao_app/views.py
+++ votacao_app/views.py
@@ -15,9 +15,6 @@ def home(request):
 
 def verificar(request):
     print(1)
-    user = (request.POST.get('cod1'),request.POST.get('cod2')   )
-    # user += request.POST.get('cod2')   
-    # user += request.POST.get('cod3')   
-    # user += request.POST.get('cod4')   
+    user = (request.POST.get('cod1'),request.POST.get('cod2'),request.POST.get('cod3'),request.POST.get('cod4')   )
     print(user)
     return render(request,'verificar_senha.html')
