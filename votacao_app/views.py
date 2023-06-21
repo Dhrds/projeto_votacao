a1d56a9f36e94f410b81d27eb78cb18a8efd6e09
Douglas <dhrds1996@gmail.com>
2023-06-21 15:58:39 -0300

    .

--- votacao_app/views.py
+++ votacao_app/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render , HttpResponse
 from django.core.mail import send_mail 
 from django.contrib.auth.decorators import login_required 
 from votacao_app.forms import Codigo
+import random 
 
 @login_required
 def home(request):
-    user = request.POST.get('email') 
-    print(user)
-    #send_mail('assunto','corpo','dhrds1996@gmail.com',['dhrds1996@hotmail.com',])
+    email = request.POST.get('email') 
+    print(email)
+    num = random.randint(1,9999)
+    print(num)
+    send_mail('assunto',num,'dhrds1996@gmail.com',[email,])
     return render(request,'login.html')
 
 def verificar(request):
     print(1)
-    user = request.POST.get('cod1')   
+    user = (request.POST.get('cod1'),request.POST.get('cod2')   )
+    # user += request.POST.get('cod2')   
+    # user += request.POST.get('cod3')   
+    # user += request.POST.get('cod4')   
     print(user)
     return render(request,'verificar_senha.html')
