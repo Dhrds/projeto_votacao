01de4ba7ab5c51c2f674636efdca3ed25a83fa7e
Dhrds <dhrds1996@gmail.com>
2023-06-26 22:30:55 -0300

    .

--- votacao_app/views.py
+++ votacao_app/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .models import Validacao ,EmailConfirmacao
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.hashers import check_password
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate ,login
+from .models import Validacao, EmailConfirmacao
+from django.contrib.auth import authenticate, login
 import random
 
 # @login_required
+
+
 def home(request):
     cookies = request.COOKIES
     email = request.POST.get('email')
     reponse = render(request, 'verificar_senha.html')
-    reponse.set_cookie('email',email)
-    print(cookies['email'])
+    reponse.set_cookie('email', email)
     emails = EmailConfirmacao.objects.values_list('emails')
-    x=[]
+    x = []
     for mail in emails:
         x.append(mail[0])
     msg = x
@@ -25,27 +23,24 @@ def home(request):
         num = random.randint(1000, 9999)
         num = str(num)
         cod = Validacao.objects.get(email=email)
-        cod.codido = num  
-        cod.save()   
-        print(cod.email,cod.codido)          
-        send_mail('assunto',num,'votacaoproz@gmail.com',[email,])
-        
+        cod.codido = num
+        cod.save()
+        send_mail('assunto', num, 'votacaoproz@gmail.com', [email, ])
         reponse = render(request, 'verificar_senha.html')
-        reponse.set_cookie('email',email)
-        return reponse  
-        
+        reponse.set_cookie('email', email)
+        return reponse
+
     else:
-        print('aqui',email)
         reponse = render(request, 'login.html', {'msg': 'teste'})
-        reponse.set_cookie('email',email)
-        return reponse  
+        reponse.set_cookie('email', email)
+        return reponse
 
 
 def verificar(request):
     cookies = request.COOKIES
-    email =cookies['email']
+    email = cookies['email']
     emails = EmailConfirmacao.objects.values_list('emails')
-    x=[]    
+    x = []
     for mail in emails:
         x.append(mail[0])
     if email in x:
@@ -55,16 +50,11 @@ def verificar(request):
         cod3 = request.POST.get('cod3', 'None')
         cod4 = request.POST.get('cod4', 'None')
         cods = cod1 + cod2 + cod3 + cod4
-        print(cods,cod.email, cod.codido)
         if cod.codido == cods:
-            # usuario = authenticate(request, username=username, password=password)
-
             return HttpResponse('Bem vindo!')
         else:
-            print(2)
-            return render(request,'verificar_senha.html')
+            return render(request, 'verificar_senha.html')
     else:
         reponse = render(request, 'login.html', {'msg': 'teste'})
-        reponse.set_cookie('email',cookies['email'])
-        return reponse  
-
+        reponse.set_cookie('email', cookies['email'])
+        return reponse
