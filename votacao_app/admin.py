c0d374a51c068557d42bfaab3ac83943083a7e82
Dhrds <dhrds1996@gmail.com>
2023-07-02 19:12:23 -0300

    .

--- votacao_app/admin.py
+++ votacao_app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Aluno,Grupo,Votacao ,Votacao
+from .models import Aluno,Grupo,Votacao 
 
 # Register your models here.
 admin.site.register(Aluno)
 admin.site.register(Grupo)
-admin.site.register(Votacao)
 admin.site.register(Votacao)
