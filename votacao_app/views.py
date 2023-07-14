5ad9caee16ef2203326f080ed1c9f3505e755aee
Douglas <dhrds1996@gmail.com>
2023-07-14 12:29:29 -0300

    função email a fazer

--- votacao_app/views.py
+++ votacao_app/views.py
@@ -6,6 +6,55 @@ from django.contrib.auth import authenticate, login
 from django.http import Http404
 import random
 
+def envia_email(email):
+    aluno = Aluno.objects.get(email=email)
+    num = random.randint(1000, 9999)
+    num = str(num)
+    aluno.codigo = num
+    aluno.save()
+    html = f'''
+            <html>
+                <body>
+                    <div style="box-sizing:border-box;width:600px;display:block;background-color:#fff;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;padding:40px;border-radius:2px;border:solid 1px #efefef;text-align:center;margin-top:1rem"
+    id="m_-8812820040621664389__body__">
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">Oi,
+        {aluno.nome}</div>
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
+        <h1
+            style="text-align:center;font-size:20px;color:#323232;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0;line-height:24px">
+            Seu código para autenticação</h1>
+    </div>
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
+        <p
+            style="text-align:center;margin:0;padding:0;color:#323232;line-height:22px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif">
+            Recebemos uma solicitação de acesso à sua conta. Utilize o código abaixo para confirmar:</p>
+    </div>
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
+        <center>
+            <div
+                style="background:#f9f9f9;padding-top:9px;padding-bottom:7px;padding-left:39px;padding-right:36px;width:163px;height:41px">
+                <p
+                    style="text-align:center;margin:0;padding:0;color:#323232;line-height:25px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;font-size:18px;font-weight:bold;letter-spacing:4.5px;padding-top:9px">
+                    {num}</p>
+            </div>
+        </center>
+    </div>
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
+        <p
+            style="text-align:center;margin:0;padding:0;color:#999999;line-height:16px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;font-size:12px">
+            Código válido para acesso.</p>
+    </div>
+    <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
+        <hr style="border:0;height:1px;background-color:#e1e1e1;width:429px">
+    </div>
+
+</div>
+                </body>
+            </html>
+            '''
+    send_mail('Código de Verificação', '',
+                'votacaoproz@gmail.com', [email, ], html_message=html)
+
 
 def home(request):
     msg = request.session.get('messages', None)
@@ -15,11 +64,7 @@ def home(request):
 
 def verificar(request):
     email = request.session.get('email', None)
-    emails = Aluno.objects.values_list('email')
-    x = []
-    for mail in emails:
-        x.append(mail[0])
-    if email in x:
+    if Aluno.objects.values_list('email').exists():
         cod = Aluno.objects.get(email=email)
         cod1 = request.POST.get('cod1', 'None')
         cod2 = request.POST.get('cod2', 'None')
@@ -85,54 +130,7 @@ def check(request):
         num = str(num)
         aluno.codigo = num
         aluno.save()
-        html = f'''
-                <html>
-                    <body>
-                        <div style="box-sizing:border-box;width:600px;display:block;background-color:#fff;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;padding:40px;border-radius:2px;border:solid 1px #efefef;text-align:center;margin-top:1rem"
-        id="m_-8812820040621664389__body__">
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">Oi,
-            {aluno.nome}</div>
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
-            <h1
-                style="text-align:center;font-size:20px;color:#323232;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0;line-height:24px">
-                Seu código para autenticação</h1>
-        </div>
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
-            <p
-                style="text-align:center;margin:0;padding:0;color:#323232;line-height:22px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif">
-                Recebemos uma solicitação de acesso à sua conta. Utilize o código abaixo para confirmar:</p>
-        </div>
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
-            <center>
-                <div
-                    style="background:#f9f9f9;padding-top:9px;padding-bottom:7px;padding-left:39px;padding-right:36px;width:163px;height:41px">
-                    <p
-                        style="text-align:center;margin:0;padding:0;color:#323232;line-height:25px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;font-size:18px;font-weight:bold;letter-spacing:4.5px;padding-top:9px">
-                        {num}</p>
-                </div>
-            </center>
-        </div>
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
-            <p
-                style="text-align:center;margin:0;padding:0;color:#999999;line-height:16px;font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;font-size:12px">
-                Código válido para acesso.</p>
-        </div>
-        <div style="font-family:Verdana,'Helvetica Neue',HelveticaNeue,Helvetica,Arial,sans-serif;margin:0 0 30px 0">
-            <hr style="border:0;height:1px;background-color:#e1e1e1;width:429px">
-        </div>
-
-    </div>
-                    </body>
-                </html>
-                '''
-        send_mail('Código de Verificação', '',
-                  'votacaoproz@gmail.com', [email, ], html_message=html)
-#         send_mail('Código de Verificação', f'''Valide o código no sistema appVotação!
-
-# {num}
-
-
-# ''', 'votacaoproz@gmail.com', [email, ])
+        envia_email(aluno.email)
         print(num)
         reponse = render(request, 'verificar_senha.html')
         request.session['email'] = email
